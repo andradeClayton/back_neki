ab89b66e088d8a705cc7367457a781603a978030
andradeClayton <sussatere@gmail.com>
2021-09-29 15:03:33 -0300

    primeiro git do backend NEKI

---
+++ src/main/java/com/neki/backend/app_neki/AppNekiApplication.java
@@ -0,0 +1,13 @@
+package com.neki.backend.app_neki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppNekiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppNekiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neki/backend/app_neki/AppNekiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neki.backend.app_neki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppNekiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
